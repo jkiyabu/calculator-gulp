ed59acc88a60c1d48ad0dd19844420e3b4d62959
Jayeson Kiyabu and Patrick McGreevy <jayeson@>
2017-03-13 16:04:25 -0700

    add list of package installs

---
+++ JS package installs.sh
@@ -0,0 +1,24 @@
+npm init
+npm install gulp --save-dev
+npm install browserify --save-dev
+npm install gulp-concat --save-dev
+npm install vinyl-source-stream --save-dev
+npm install gulp-concat --save-dev
+npm install gulp-uglify --save-dev
+npm install gulp-util --save-dev
+npm install del --save-dev
+npm install jshint --save-dev
+npm install gulp-jshint --save-dev
+bower init
+bower install jquery --save
+bower install bootstrap --save
+bower install moment --save
+npm install bower-files --save-dev
+npm install browser-sync --save-dev
+
+
+# Global
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo 'export PATH=/usr/local/bin:$PATH' >> ~/.bash_profile
+brew install node
+npm install bower -g
